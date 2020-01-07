9be7bbea49e95076b5f4271eaaea3600f025a3f6
Talbekk <mt.campbell@outlook.com>
2020-01-07 16:54:36 +0000

    adds basic book attribute checks

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,16 +1,27 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
 
     private Book book;
 
     @Before
     public void before(){
-        book = new Book();
+        book = new Book("The Lord of the Rings", "JRR Tolkien", "Fantasy");
     }
     @Test
-    public void hasTitle(){}
-
+    public void hasTitle() {
+        assertEquals("The Lord of the Rings", book.getTitle());
+    }
+    @Test
+    public void hasAuthor(){
+        assertEquals("JRR Tolkien", book.getAuthor());
+    }
+    @Test
+    public void hasGenre(){
+        assertEquals("Fantasy", book.getGenre());
+    }
 
 }
