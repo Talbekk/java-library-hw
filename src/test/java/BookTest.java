22e447cc0c136d876005721f22bc5115bbd594c1
Talbekk <mt.campbell@outlook.com>
2020-01-07 16:48:06 +0000

    setup book files

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class BookTest {
+
+    private Book book;
+
+    @Before
+    public void before(){
+        book = new Book();
+    }
+    @Test
+    public void hasTitle(){}
+
+
+}
