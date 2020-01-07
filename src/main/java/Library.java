81b09089989c838442041c23fbaf1b0457fc74b9
Talbekk <mt.campbell@outlook.com>
2020-01-07 17:04:01 +0000

    adds basic library attribute checks

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+
 public class Library {
 
+        private String name;
+        private String location;
+        private ArrayList<Book> collection;
 
-    public Library() {
+    public Library(String name, String location) {
+
+        this.name = name;
+        this.location = location;
+        this.collection = new ArrayList<Book>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public int bookCount(){
+        return this.collection.size();
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class LibraryTest {
 
@@ -7,6 +10,22 @@ public class LibraryTest {
     @Before
 
     public void before(){
-        library = new Library();
+        library = new Library("Alexandria", "Egypt");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Alexandria", library.getName());
+    }
+
+    @Test
+    public void hasLocation(){
+        assertEquals("Egypt", library.getLocation());
+    }
+
+    @Test
+
+    public void collectionStartsEmpty(){
+        assertEquals(0, library.bookCount());
     }
 }
